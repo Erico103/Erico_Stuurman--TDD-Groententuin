314f76b82b28b706a2f5f57ed0842cf478ed5e46
Erico Stuurman <info@ericoonline.nl>
2021-10-07 18:16:48 +0200

    Tryout

---
+++ farm.js
@@ -0,0 +1,40 @@
+const getYieldForPlant = (object) => {
+  let soilClayFactor = 100;
+  let sunFactor = 100;
+  let windFactor = 100;
+    const corn = 30
+        return corn
+     }
+
+
+
+const getYieldForCrop = (object) => {
+  let soilClayFactor = 100;
+  let sunFactor = 100;
+  let windFactor = 100;
+ }
+
+
+const getTotalYield = () => { }
+
+
+const getCostsForCrop = () => { }
+
+
+const getRevenueForCrop = () => { }
+
+
+const getProfitForCrop = () => { }
+
+
+const getTotalProfit = () => { }
+
+module.exports = {
+    getYieldForPlant,
+    getYieldForCrop,
+    getTotalYield,
+    getCostsForCrop,
+    getRevenueForCrop,
+    getProfitForCrop,
+    getTotalProfit
+  };
